76cb17b48dca26cdb10ebd9ed129dbd25d34c747
Daniel Ruoso <daniel@ruoso.com>
2010-08-19 22:37:11 -0300

    comments for kthakore

--- lib/Jogo/View/FilledRect.pm
+++ lib/Jogo/View/FilledRect.pm
@@ -6,6 +6,9 @@ use SDLx::Surface;
 use SDL::Rect;
 use base 'Jogo::Type::Rect';
 
+# initializes the object. We assume we need color, the main surface
+# and the camera.  All units (x, y, w, h) are in the game unit, not
+# pixels. The camera is the thing responsible for unit conversion.
 sub _init {
     my $self = shift;
     die "Need camera and main surface on initialization"
@@ -14,6 +17,8 @@ sub _init {
     $self->_init_rect;
 }
 
+# In FilledRect we create the surface with the collor, so it's a
+# filled rect.
 sub _init_surface {
     my ($self) = @_;
     $self->{surface} =
@@ -26,6 +31,8 @@ sub _init_surface {
     return 1;
 }
 
+# _init_rect sets $self->{rect_obj} in order to avoid creating a new
+# rect for every render call.
 sub _init_rect {
     my ($self) = @_;
     $self->{rect_obj} =
@@ -36,6 +43,9 @@ sub _init_rect {
     return 1;
 }
 
+
+# w and h are overriden to rebuild the surface and the rect. This only
+# happens when someone re-sets it manually.
 sub w {
     my $self = shift;
     $self->SUPER::w(@_);
@@ -45,7 +55,6 @@ sub w {
     }
     return $self->{w};
 }
-
 sub h {
     my $self = shift;
     $self->SUPER::h(@_);
@@ -56,6 +65,9 @@ sub h {
     return $self->{h};
 }
 
+
+# This is where the actual rendering happens, it builds the target
+# rect by using the camera translate_x_y_w_h method.
 sub render {
     my ($self) = @_;
 
@@ -70,12 +82,16 @@ sub render {
     return 1;
 }
 
+# This is what is called when the camera dpi is changed It rebuilds
+# the surface and the source rect.
 sub zoomed_event_fired {
     my $self = shift;
     $self->_init_surface;
     $self->_init_rect;
 }
 
+# This receives the moved information. Simply updates x and y to be
+# used in the next render call.
 sub moved_event_fired {
     my ($self, $ev) = @_;
     $self->$_($ev->new_point->$_) for
